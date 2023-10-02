4629301a72b9be7131f5c8b51a6b098312cffb66
grigorii.davydenko <grigorydavydenko@yandex.ru>
2023-10-02 19:30:18 +0300

    added conversion from NFA to DFA

---
+++ from-NFA-to-DFA/from-NFA-to-DFA.cpp
@@ -0,0 +1,75 @@
+#include <queue>
+
+#include "../eps-transitions/eps-transitions.h"
+
+const uint32_t kAlphabetSize = 26;
+const char kFirstCharacter = 'a';
+
+class DFA {
+ protected:
+  struct Node {
+    std::map<char, uint32_t> edges;
+    bool terminal;
+    Node() : terminal(false) {}
+  };
+
+  // start: 0
+  std::vector<Node> nodes_;
+
+ public:
+  DFA(NFA nfa) {
+    BuildDFA(nfa.start_, nfa.nodes_);
+  }
+
+ private:
+  void BuildDFA(uint32_t nfa_start, std::vector<NFA::Node> nfa_nodes) {
+    // sets of node index in new array, which connected with sets of NFA nodes
+    std::map<std::set<uint32_t>, uint32_t> index_by_set;
+    std::map<uint32_t, std::set<uint32_t>> set_by_index;
+
+    bool terminal;
+    uint32_t node_index;
+    std::set<uint32_t> new_node;
+    std::queue<uint32_t> queue;
+
+    new_node.insert(nfa_start);
+    index_by_set.insert(std::pair(new_node, 0));
+    set_by_index.insert(std::pair(0, new_node));
+    queue.push(0);
+
+    nodes_.emplace_back();
+    if (nfa_nodes[nfa_start].terminal) {
+      nodes_[nodes_.size() - 1].terminal = true;
+    }
+
+    while (!queue.empty()) {
+      node_index = queue.back();
+      queue.pop();
+
+      for (char chr = kFirstCharacter; chr < kFirstCharacter + kAlphabetSize; ++chr) {
+        new_node.clear();
+        terminal = false;
+
+        for (uint32_t nfa_node : set_by_index[node_index]) {
+          new_node.insert(nfa_nodes[nfa_node].edges[chr].begin(),
+                          nfa_nodes[nfa_node].edges[chr].end());
+          if (nfa_nodes[nfa_start].terminal) {
+            terminal = true;
+          }
+        }
+
+        if (!index_by_set.count(new_node)) {
+          index_by_set.insert(std::pair(new_node, nodes_.size()));
+          set_by_index.insert(std::pair(nodes_.size(), new_node));
+          queue.push(nodes_.size());
+
+          nodes_.emplace_back();
+          if (terminal) {
+            nodes_[nodes_.size() - 1].terminal = true;
+          }
+        }
+        nodes_[node_index].edges[chr] = index_by_set[new_node];
+      }
+    }
+  }
+};
